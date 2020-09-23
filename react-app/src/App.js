fd77221abc4f400acdaab30b0e2d943e18790a0c
Jhonatan Tomimatsu <jhonatan.tft@gmail.com>
2020-09-22 23:17:47 -0300

    first commit

---
+++ react-app/src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+import FirstComponent from "./components/FirstComponent";
+import './styles/main.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <FirstComponent/>
+                
+                <h1>change</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ react-app/src/components/FirstComponent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FirstComponent = props => {
+    return (
+        <div>
+            This is my first example component
+            <div class="container">
+                oi karoline pfosadkpod
+                dfasdas
+            </div>
+        </div>
+    );
+};
+
+FirstComponent.propTypes = {};
+
+export default FirstComponent;
+
+
