617ecfe2694815b52015e5521797b5af8742b196
Jhonatan Tomimatsu <jhonatan.tft@gmail.com>
2020-10-04 05:23:00 -0300

    feat: define api endpoint

--- express-app/routes/index.js
+++ express-app/routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const similarityInferenceController = require('../controller/inference.js')
+
+// Mock user
+const mockUserPreferences = {
+  cuisineType: 'Japonesa',
+  price: 50,
+  glutenFree: false,
+  sugarFree: false,
+  lactoseFree: false,
+  soyFree: false,
+  thematic: false,
+  rating: 1
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json(similarityInferenceController.getDataBase)
 });
+
+router.post('/', function (req, res) {
+  const userPreferences = req.body
+  res.send(similarityInferenceController.getSimilars(userPreferences || mockUserPreferences))
+})
 
 module.exports = router;
